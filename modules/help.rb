67e84acdaf9a2698e00c3c46ce535ee92c591cd2
Spotlight <awesomebing1@gmail.com>
2017-06-06 10:28:39 -0500

    Update to support properly prefix

--- modules/help.rb
+++ modules/help.rb
@@ -16,38 +16,37 @@ module SerieBot
       help << "`#{Config.prefix}dns` will reply with the DNS settings for RiiConnect24. \n"
       help << "`#{Config.prefix}about` will tell you information about the bot.\n"
       help << "Need to patch your `nwc24msg.cfg` file? Just DM me the file, and I'll patch it for you."
-      extrahelp = ""
+      extra_help = ''
       if Helper.is_admin?(event.user) || Helper.is_moderator?(event) || Helper.is_developer?(event)
-        extrahelp << "\n\n**__Mod commands__**\n"
-        extrahelp << "As this RiiConnect24 bot is a stripped down version of Yuu-Chan/Serie-Bot, you have a limited option of some moderation commands.\n"
-        extrahelp << "\n"
-        extrahelp << "**General commands**\n"
-        extrahelp << "`#{Config.prefix}ignore @user`/`#{Config.prefix}unignore @user` will respectively ignore and unignore the specified user.\n"
-        extrahelp << ""
+        extra_help << "\n\n**__Mod commands__**\n"
+        extra_help << "As this RiiConnect24 bot is a stripped down version of Yuu-Chan/Serie-Bot, you have a limited option of some moderation commands.\n"
+        extra_help << "\n"
+        extra_help << "**General commands**\n"
+        extra_help << "`#{Config.prefix}ignore @user`/`#{Config.prefix}unignore @user` will respectively ignore and unignore the specified user.\n"
       end
       if Helper.is_admin?(event.user) || Helper.is_bot_helper?(event) || Helper.is_developer?(event)
-        extrahelp << "\n\n**Developers:**\n"
-        extrahelp << "`#{Config.prefix}setavatar <file/URL>` will change the avatar to the provided URL/image.\n"
-        extrahelp << "`#{Config.prefix}status <status>` changes the status of the bot to one of the options of idle, dnd, invisible or online.\n"
-        extrahelp << "`#{Config.prefix}dump <id>` will dump all messages from the channel represented by the specified ID.\n"
-        extrahelp << "`#{Config.prefix}clear <num>` will clear <num> messages from chat.\n"
-        extrahelp << "`#{Config.prefix}kick @user` will kick @user from the server.\n"
-        extrahelp << "`#{Config.prefix}ban @user` will ban @user from the server.\n"
-        extrahelp << "`#{Config.prefix}lockdown <minutes>` will lockdown the channel. If specified, the channel will unlock after <minutes> or not at all.\n"
-        extrahelp << "`#{Config.prefix}unlockdown` will remove the lockdown from the channel.\n"
+        extra_help << "\n\n**Developers:**\n"
+        extra_help << "`#{Config.prefix}setavatar <file/URL>` will change the avatar to the provided URL/image.\n"
+        extra_help << "`#{Config.prefix}status <status>` changes the status of the bot to one of the options of idle, dnd, invisible or online.\n"
+        extra_help << "`#{Config.prefix}dump <id>` will dump all messages from the channel represented by the specified ID.\n"
+        extra_help << "`#{Config.prefix}clear <num>` will clear <num> messages from chat.\n"
+        extra_help << "`#{Config.prefix}kick @user` will kick @user from the server.\n"
+        extra_help << "`#{Config.prefix}ban @user` will ban @user from the server.\n"
+        extra_help << "`#{Config.prefix}lockdown <minutes>` will lockdown the channel. If specified, the channel will unlock after <minutes> or not at all.\n"
+        extra_help << "`#{Config.prefix}unlockdown` will remove the lockdown from the channel.\n"
       end
       if Helper.is_admin?(event.user) || Helper.is_bot_helper?(event)
-        extrahelp << "\n\n**Admins**\n"
-        extrahelp << "`#{Config.prefix}eval <code>` will evaluate the specified Ruby string. !!! USE WITH CARE !!!\n"
-        extrahelp << "`#{Config.prefix}bash <command>` will run the specified command in a bash shell. As before, !!! USE WITH CARE !!!\n"
-        extrahelp << "`#{Config.prefix}shutdown` will do exactly as the name suggests to the bot.\n"
+        extra_help << "\n\n**Admins**\n"
+        extra_help << "`#{Config.prefix}eval <code>` will evaluate the specified Ruby string. !!! USE WITH CARE !!!\n"
+        extra_help << "`#{Config.prefix}bash <command>` will run the specified command in a bash shell. As before, !!! USE WITH CARE !!!\n"
+        extra_help << "`#{Config.prefix}shutdown` will do exactly as the name suggests to the bot.\n"
       end
       begin
         event.user.pm(help)
         if Helper.is_admin?(event.user) || Helper.is_moderator?(event) || Helper.is_developer?(event)
-          event.user.pm(extrahelp)
+          event.user.pm(extra_help)
         end
-        event.respond("Check your DMs!")
+        event.respond('Check your DMs!')
       rescue Discordrb::Errors::NoPermission
         event.respond("❌ Sorry, but it looks like you're blocking DMs.")
       end
