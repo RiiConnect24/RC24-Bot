04e30fbb23637c65549f1d188971eadee2a72dce
Spotlight <awesomebing1@gmail.com>
2017-09-10 10:33:03 -0500

    Add CircleCI integration

---
+++ src/xyz/rc24/bot/commands/tools/Error.java
@@ -0,0 +1,56 @@
+package xyz.rc24.bot.commands.tools;
+
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Artu.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import com.jagrosh.jdautilities.commandclient.Command;
+import com.jagrosh.jdautilities.commandclient.CommandEvent;
+import net.dv8tion.jda.core.Permission;
+import xyz.rc24.bot.utils.CodeManager;
+
+/**
+ * Looks up errors using the Wiimmfi API.
+ * @author Artu
+ */
+
+public class Error extends Command {
+    private CodeManager manager;
+    public Error(CodeManager manager) {
+        this.manager = manager;
+        this.name = "error";
+        this.help = "Turns the bot off.";
+        this.category = new Command.Category("Admins");
+        this.botPermissions = new Permission[]{Permission.MESSAGE_WRITE};
+        this.userPermissions = new Permission[]{Permission.MESSAGE_WRITE};
+        this.ownerCommand = true;
+        this.guildOnly = false;
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        manager.destroy();
+        event.getTextChannel().sendMessage("Done! Cya \uD83D\uDC4B").complete();
+        event.getJDA().shutdown();
+    }
+}
