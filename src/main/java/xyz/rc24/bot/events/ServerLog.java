030dc26b67cb9d3cce7ff77d945ce70cd2743c93
Artuto <artutogamer@gmail.com>
2018-02-17 22:21:28 -0600

    serverlog fix

--- src/main/java/xyz/rc24/bot/events/ServerLog.java
+++ src/main/java/xyz/rc24/bot/events/ServerLog.java
@@ -18,44 +18,50 @@ import java.awt.*;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 
-public class ServerLog extends ListenerAdapter {
+public class ServerLog extends ListenerAdapter
+{
     private ServerConfigManager manager;
     private static final Logger logger = LoggerFactory.getLogger(ServerLog.class);
 
-    public ServerLog() {
+    public ServerLog()
+    {
         this.manager = new ServerConfigManager();
         logger.info("Tracking :eyes:");
     }
 
     @Override
-    public void onGuildBan(GuildBanEvent event) {
+    public void onGuildBan(GuildBanEvent event)
+    {
         EmbedBuilder builder = getEmbed("A user was banned from the server!",
-                "#D32F2F",
-                event.getUser());
+                "#D32F2F", event.getUser());
 
         sendEmbed(new LogType[]{LogType.SERVER, LogType.MOD}, builder, event);
     }
 
     @Override
-    public void onGuildUnban(GuildUnbanEvent event) {
+    public void onGuildUnban(GuildUnbanEvent event)
+    {
         EmbedBuilder builder = getEmbed("A user was unbanned from the server!",
-                "#4CAF50",
-                event.getUser());
+                "#4CAF50", event.getUser());
 
         sendEmbed(new LogType[]{LogType.SERVER, LogType.MOD}, builder, event);
     }
 
     @Override
-    public void onGuildMemberLeave(GuildMemberLeaveEvent event) {
-        try {
-            if (!event.getGuild().getBans().complete().contains(event.getUser())) {
+    public void onGuildMemberLeave(GuildMemberLeaveEvent event)
+    {
+        try
+        {
+            if (!(event.getGuild().getBanList().complete().contains(event.getUser())))
+            {
                 EmbedBuilder builder = getEmbed("A user left the server!",
-                        "#FFEB3B",
-                        event.getUser());
+                        "#FFEB3B", event.getUser());
 
                 sendEmbed(new LogType[]{LogType.SERVER}, builder, event);
             }
-        } catch (InsufficientPermissionException e) {
+        }
+        catch(InsufficientPermissionException e)
+        {
             EmbedBuilder builder = new EmbedBuilder();
             builder.setTitle("Warning!");
             builder.setDescription("I don't have the Ban Members permission. I won't ban anyone, but without it I can't tell if anyone's leaving, or if it was a ban.");
@@ -68,15 +74,16 @@ public class ServerLog extends ListenerAdapter {
     }
 
     @Override
-    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+    public void onGuildMemberJoin(GuildMemberJoinEvent event)
+    {
         EmbedBuilder builder = getEmbed("A user just joined the server!",
-                "#00C853",
-                event.getUser());
+                "#00C853", event.getUser());
 
         sendEmbed(new LogType[]{LogType.SERVER}, builder, event);
     }
 
-    private EmbedBuilder getEmbed(String title, String color, User user) {
+    private EmbedBuilder getEmbed(String title, String color, User user)
+    {
         EmbedBuilder builder = new EmbedBuilder();
         builder.setTitle(title);
         builder.setDescription("User: " + user.getAsMention() + " | " + user.getName() + "#" + user.getDiscriminator() + "\n" +
@@ -84,20 +91,24 @@ public class ServerLog extends ListenerAdapter {
         builder.setColor(Color.decode(color));
         builder.setFooter("Current time: ", null);
         builder.setTimestamp(Instant.now());
+
         return builder;
     }
 
-    private void sendEmbed(LogType[] logTypes, EmbedBuilder embed, GenericGuildEvent guildEvent) {
+    private void sendEmbed(LogType[] logTypes, EmbedBuilder embed, GenericGuildEvent guildEvent)
+    {
         Long guildID = guildEvent.getGuild().getIdLong();
 
-        for (LogType logType : logTypes) {
-            try {
+        for(LogType logType : logTypes)
+        {
+            try
+            {
                 Long channelID = manager.getLog(logType, guildID);
-                if (channelID == null) return;
-                guildEvent.getJDA()
-                        .getTextChannelById(channelID)
-                        .sendMessage(embed.build()).complete();
-            } catch (InsufficientPermissionException e) {
+                if(channelID==null) return;
+                guildEvent.getJDA().getTextChannelById(channelID).sendMessage(embed.build()).queue();
+            }
+            catch (InsufficientPermissionException e)
+            {
                 // Remove the log from the config, since it's invalid.
                 manager.disableLog(logType, guildID);
             }
