ae5d3c565d766bda5ec4ed2e01094329750c2583
Artuto <artutogamer@gmail.com>
2018-04-21 08:44:24 -0500

    Detect 503's

--- src/main/java/xyz/rc24/bot/commands/tools/MailParseListener.java
+++ src/main/java/xyz/rc24/bot/commands/tools/MailParseListener.java
@@ -77,6 +77,8 @@ public class MailParseListener extends ListenerAdapter
 
                         if(response.code()==400)
                             throw new IOException("Invalid file! Make sure you sent the correct file!");
+                        if(response.code()==503)
+                             throw new IOException("The server is now currently under maintenance. Please wait some time and try again.");
 
                         String content = response.body().string();
                         Writer output = new BufferedWriter(new FileWriter("nwc24msg.cfg", true));
