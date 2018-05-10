06a03e7c3c4d30475312c685ff0b6c2bd3e79892
Artuto <artutogamer@gmail.com>
2018-05-10 10:19:21 -0500

    Added Wiiware patcher command

---
+++ src/main/java/xyz/rc24/bot/commands/wii/WiiWare.java
@@ -0,0 +1,53 @@
+package xyz.rc24.bot.commands.wii;
+ 
+/*
+ * The MIT License
+ *
+ * Copyright 2017 RiiConnect24 and its contributors.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.core.Permission;
+import xyz.rc24.bot.commands.Categories;
+ 
+/**
+ * @author Artuto
+ */
+ 
+public class WiiWare extends Command
+{
+    public WiiWare()
+    {
+        this.name = "wiiware";
+        this.help = "Lets you know the URL to the WiiWare patcher.";
+        this.category = Categories.WII;
+        this.botPermissions = new Permission[]{Permission.MESSAGE_WRITE};
+        this.userPermissions = new Permission[]{Permission.MESSAGE_WRITE};
+        this.guildOnly = false;
+    }
+ 
+    @Override
+    protected void execute(CommandEvent event)
+    {
+       event.reply("Check out the patcher here: https://github.com/RiiConnect24/auto-wiiware-patcher/releases");
+    }
+}
