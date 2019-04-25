64f1f9e91d11896f0e2e4f5a4376557e147a3c0d
Artuto <artutogamer@gmail.com>
2019-04-25 15:51:16 -0500

    [CI-SKIP] update regex for switch codes

--- src/main/java/xyz/rc24/bot/commands/wii/CodeCmd.java
+++ src/main/java/xyz/rc24/bot/commands/wii/CodeCmd.java
@@ -48,8 +48,7 @@ public class CodeCmd extends Command
     private final BotCore core;
     private final CodeDataManager dataManager;
 
-    private final Pattern FULL_PATTERN = Pattern.compile("(\\w+)\\s+(.+?)\\s+(\\d{4}[-\\s]\\d{4}[-\\s]\\d{4}" +
-            "(?:[-\\s]\\d{4})?|\\w+)$", Pattern.MULTILINE); // thanks Dismissed
+    private final Pattern FULL_PATTERN = Pattern.compile("(\\w+)\\s+(.+?)\\s+((?:\\d{4}|SW)[-\\s]\\d{4}[-\\s]\\d{4}(?:[-\\s]\\d{4})?|\\w+)$", Pattern.MULTILINE); // thanks Dismissed
     private final Pattern REMOVE_PATTERN = Pattern.compile("(\\w+)\\s+(.+?)$", Pattern.MULTILINE);
 
     public CodeCmd(Bot bot)
