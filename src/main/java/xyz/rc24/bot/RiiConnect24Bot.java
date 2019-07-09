258199b77c29af408e9ba060ea8b9f3d02323e78
Artuto <artutogamer@gmail.com>
2019-07-08 22:59:54 -0500

    Implemented DataDog stats reporting (i dont even know if this will work tbh)

--- src/main/java/xyz/rc24/bot/RiiConnect24Bot.java
+++ src/main/java/xyz/rc24/bot/RiiConnect24Bot.java
@@ -38,11 +38,11 @@ public class RiiConnect24Bot
 
     public static void main(String[] args) throws LoginException
     {
-		// Sentry
+        // Sentry
         System.setProperty("sentry.stacktrace.app.packages", "xyz.rc24.bot");
         System.setProperty("sentry.release", Const.VERSION);
-		
-		// JDA
+
+        // JDA
         RestAction.setPassContext(true); // enable context by default
         RestAction.DEFAULT_FAILURE = Throwable::printStackTrace;
 
