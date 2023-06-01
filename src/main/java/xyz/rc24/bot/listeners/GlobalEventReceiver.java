2ac6c899394bd9f6cce7c65dbee205b8dcd787ae
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2023-06-01 19:19:32 -0400

    Fix bot presence

--- src/main/java/xyz/rc24/bot/listeners/GlobalEventReceiver.java
+++ src/main/java/xyz/rc24/bot/listeners/GlobalEventReceiver.java
@@ -2,6 +2,9 @@ package xyz.rc24.bot.listeners;
 
 import com.thegamecommunity.discord.event.receiver.DefaultDiscordEventReceiver;
 
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import xyz.rc24.bot.RiiConnect24Bot;
 import xyz.rc24.bot.commands.Dispatcher;
 import xyz.rc24.bot.commands.RiiContext;
@@ -15,6 +18,12 @@ public class GlobalEventReceiver extends DefaultDiscordEventReceiver {
 	@Override
 	public RiiContext createContext(Object o) {
 		return new RiiContext(o);
+	}
+	
+	@Override
+	public void onReady(ReadyEvent e) {
+		super.onReady(e);
+		RiiConnect24Bot.getInstance().jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.listening("commands"));
 	}
 	
 }
